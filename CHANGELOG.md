2c396fc37db39951c5c6afbfcd737ceaa17a02a2
Adam Zaninovich <adam.zaninovich@gmail.com>
2013-02-26 11:38:14 -0800

    compatible with 1.8.7, ready for v0.0.2 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,16 +1,20 @@
-## v0.0.3 (far future)
+## v0.0.4 (far future)
 
 ### Todo
 
 * Pluggable/Configurable correspondence tables
 * Pluggable languages (DSL for new languages?)
 
-## v0.0.2 (near future)
+## v0.0.3 (near future)
 
 ### Todo
 
 * Make correspondence table into its own object
 * Investigate feasability and worth of supporting Hebrew gematria
+
+## v0.0.2
+
+* Ruby 1.8.7 compatible
 
 ## v0.0.1
 
--- lib/gematria/english.rb
+++ lib/gematria/english.rb
@@ -2,9 +2,9 @@ module Gematria
   class English < Struct.new(:text)
     # applies "mispar hechrachi" method to English alphabet (http://www.inner.org/gematria/fourways.php)
     CORRESPONDENCE_TABLE = {
-      a: 1,   b: 2,   c: 3,   d: 4,   e: 5,   f: 6,   g: 7,   h: 8,   i: 9,
-      j: 10,  k: 20,  l: 30,  m: 40,  n: 50,  o: 60,  p: 70,  q: 80,  r: 90,
-      s: 100, t: 200, u: 300, v: 400, w: 500, x: 600, y: 700, z: 800
+      :a => 1,   :b => 2,   :c => 3,   :d => 4,   :e => 5,   :f => 6,   :g => 7,   :h => 8,   :i => 9,
+      :j => 10,  :k => 20,  :l => 30,  :m => 40,  :n => 50,  :o => 60,  :p => 70,  :q => 80,  :r => 90,
+      :s => 100, :t => 200, :u => 300, :v => 400, :w => 500, :x => 600, :y => 700, :z => 800
     }
 
     # Gets an array of the values for each character of text.
